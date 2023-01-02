7cd6d5ac32172f716c8cd206a8fd8e0fd332f40a
atiliodejesus <atiliodejesus.son@gmail.com>
2023-01-02 11:06:49 -0300

    first commit

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿// //********************** © KD-Studios 2023 ***************************\\
+// //************* Desenvolvido Por: Atílio De Jesus *********************\\
+// //******************* Bike System versão 0.1v **************************\\
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BikeSystem
+{
+  //Scrip da camera da moto
+  public class CameraController : MonoBehaviour
+  {
+
+    public Transform[] target;
+    public SelectControlle select;
+
+
+    void Update()
+    {
+      transform.position = target[select.index].position;
+      transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, target[select.index].localEulerAngles.y, transform.localEulerAngles.z);
+    }
+  }
+}
